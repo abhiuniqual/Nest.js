540e7e664d5a3de105af91a9e6d29591ca12126f
abhi.uniqual <abhi.uniqual@gmail.com>
2024-03-26 12:14:41 +0530

    initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll(@Param('role')) {
+    return []
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatedUser: {}) {
+    return { id, ...updatedUser };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
